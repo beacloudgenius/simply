e74a8b2083b228b05478f9fa9ef423ef12a2c7fb
Nilesh <nilesh@cloudgeni.us>
2023-02-08 10:13:48 -0800

    mermaid, asciinema, cloudflare wrangler

---
+++ embed.md
@@ -0,0 +1,28 @@
+# mermaid embed
+
+```mermaid
+%%{init: { 'theme': 'dark' } }%%
+flowchart LR
+    A[Hard edge] -->|Link text| B(Round edge)
+    B --> C{Decision}
+    C -->|One| D[Result one]
+    C -->|Two| E[Result two]
+```
+
+```mermaid
+%%{init: { 'theme': 'dark' } }%%
+pie title What Voldemort doesn't have?
+    "FRIENDS" : 2
+    "FAMILY" : 1
+    "NOSE" : 45
+```
+
+# asciinema embed
+
+```
+<div class="asciinema" src="keypair-mac.cast"></div>
+```
+
+```
+<div class="asciinema" src="powershell.cast"></div>
+```
---
+++ src/cloudflare_ghost_worker.js
@@ -0,0 +1,68 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npx wrangler dev src/cloudflare_ghost_worker.js` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npx wrangler publish src/cloudflare_ghost_worker.js --name ghost` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+// export default {
+// 	async fetch(request, env, ctx) {
+// 		return new Response("Hello World!");
+// 	},
+// };
+
+
+addEventListener("fetch", (event) => {
+	event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+	const response = await fetch(request);
+
+	// Clone the response so that it's no longer immutable
+	const newResponse = new Response(response.body, response);
+
+	// Add a custom header with a value
+	newResponse.headers.append("x-workers-hello", "Hello from Cloud Genius");
+
+	// Delete headers
+	// newResponse.headers.delete("x-header-to-delete");
+	// newResponse.headers.delete("x-header2-to-delete");
+
+	// Adjust the value for an existing header
+	newResponse.headers.set("x-powered-by", "Cloud Genius");
+
+	newResponse.headers.set(
+		"Content-Security-Policy",
+		"\
+            base-uri 'self'; \
+            object-src 'self' https://s3-us-west-2.s3.amazonaws.com; \
+            img-src 'self' ghost.org *.supabase.co *.spacergif.org *.unsplash.com *.tenor.com *.gravatar.com *.ghost.org *.cloudgenius.app cloudgenius.app thecloudseminar.com *.amazonaws.com *.ytimg.com *.vimeocdn.com *.githubusercontent.com blob: data:; \
+            media-src blob: thecloudseminar.com hls.home.cloudgeni.us hls.cloudgenius.app *.amazonaws.com youtube.com *.vimeo.com *.vimeocdn.com; \
+            script-src * data: 'unsafe-eval' 'unsafe-inline' blob: https: 'self' *.vimeo.com *.chatra.io hi.cloudgenius.app storage.googleapis.com js.stripe.com *.stripe.com *.stripe.network;\
+            frame-ancestors 'self'; \
+            form-action 'self' https://hi.cloudgenius.app/form/submit; \
+            style-src 'unsafe-inline' 'self' fonts.googleapis.com cdn.jsdelivr.net maxcdn.bootstrapcdn.com; \
+            default-src 'self' ghost.org/changelog.json ghost.org/explore/api/feed/ resources.ghost.io/resources/ghost/api/content/posts/ cdn.jsdelivr.net tcs-ghost.s3.us-west-2.amazonaws.com tenor.googleapis.com api.unsplash.com;  \
+            font-src 'self' *.gstatic.com ; \
+            frame-src 'self' js.stripe.com ghost.org/explore/ calendly.com platform.twitter.com; \
+            ",
+		// report-uri https://cloudgenius.report-uri.com;
+	);
+
+
+
+
+	newResponse.headers.set(
+		"Feature-Policy",
+		"\
+			microphone 'self'; camera 'self';\
+            ",
+
+	);
+
+	return newResponse;
+}
