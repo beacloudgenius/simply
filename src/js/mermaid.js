a7f77da7b2e051d6f59a8931b150b66ee905897d
Nilesh <nilesh@cloudgeni.us>
2023-02-07 20:10:24 -0800

    mermaid

--- src/js/mermaid.js
+++ src/js/mermaid.js
@@ -1,13 +1,12 @@
 // https://css-tricks.com/making-mermaid-diagrams-in-markdown/
 
 // import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@9/dist/mermaid.esm.min.mjs';
-import mermaid from 'mermaid';
+import mermaid from 'mermaid'
 
 // select <pre class="mermaid"> _and_ <pre><code class="language-mermaid">
-document.querySelectorAll("pre.mermaid, pre>code.language-mermaid").forEach($el => {
+document.querySelectorAll('pre.mermaid, pre>code.language-mermaid').forEach($el => {
   // if the second selector got a hit, reference the parent <pre>
-  if ($el.tagName === "CODE")
-    $el = $el.parentElement
+  if ($el.tagName === 'CODE') { $el = $el.parentElement }
   // put the Mermaid contents in the expected <div class="mermaid">
   // plus keep the original contents in a nice <details>
   $el.outerHTML = `
@@ -22,13 +21,13 @@ document.querySelectorAll("pre.mermaid, pre>code.language-mermaid").forEach($el 
 // initialize Mermaid to [1] log errors, [2] have loose security for first-party
 // authored diagrams, and [3] respect a preferred dark color scheme
 
-let isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-let mermaidTheme = (isDark) ? 'dark' : 'default';
+const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+const mermaidTheme = (isDark) ? 'dark' : 'default'
 
 mermaid.initialize({
   startOnLoad: true,
-  logLevel: "error", // [1]
-  securityLevel: "loose", // [2]
+  logLevel: 'error', // [1]
+  securityLevel: 'loose', // [2]
   theme: mermaidTheme // [3]
 })
 
